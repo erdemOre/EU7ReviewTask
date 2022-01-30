ca6140e3dd27b1078e786a89f8d4a8394aa58543
Erdem <benyunusum@gmail.com>
2022-01-30 13:48:52 +0300

    Created amazon BasePage

---
+++ src/test/java/com/pages/BasePage.java
@@ -0,0 +1,20 @@
+package com.pages;
+
+import com.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class  BasePage {
+
+    public BasePage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+
+    @FindBy(css = "#twotabsearchtextbox")
+    public WebElement searchBox;
+
+
+
+
+}
