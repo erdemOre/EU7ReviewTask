dd1645af9badec4c8da2dd5db23e605c2e1b246c
Erdem <benyunusum@gmail.com>
2022-02-04 06:24:45 +0300

    ShoppingTest, LandingPage created

---
+++ src/test/java/com/tests/ShoppingTest.java
@@ -0,0 +1,24 @@
+package com.tests;
+
+public class ShoppingTest extends TestBase{
+
+    /*
+    Task 1: The task consists on the next steps:
+    1. Go to https://www.amazon.com
+    2. Search for "hats for men" (Call from Configuration.properties file)
+    3. Add the first hat appearing to Cart with quantity 2
+    4. Open cart and assert that the total price and quantity are correct
+    5. Reduce the quantity from 2 to 1 in Cart for the item selected in the step 3
+    6. Assert that the total price and quantity has been correctly changed
+    The goal of this test is to check if you are able to automate a test of a given website,
+    but we'd like you to also demonstrate the coding quality, structure, and style of the deliverables.
+
+     */
+
+
+
+
+
+
+
+}
