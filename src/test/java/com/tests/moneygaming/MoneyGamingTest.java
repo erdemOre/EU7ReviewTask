074bd303de6891ec6aceeebc4a4ca87dabfe1994
Erdem <benyunusum@gmail.com>
2022-02-05 14:46:13 +0300

    BasePage, MoneyGamingTest, TestBase created

---
+++ src/test/java/com/tests/moneygaming/MoneyGamingTest.java
@@ -0,0 +1,22 @@
+package com.tests.moneygaming;
+
+import org.testng.annotations.Test;
+
+public class MoneyGamingTest extends TestBase {
+
+    /*
+    Task 2:
+    1. Navigate to: https://moneygaming.qa.gameaccount.com/
+    2. Click the JOIN NOW button to open the registration page
+    3. Select a title value from the dropdown
+    4. Enter your first name and surname in the form
+    5. Check the tick box with text 'I accept the Terms and Conditions and
+    certify that I am over the age of 18.'
+    6. Submit the form by clicking the JOIN NOW button
+    7. Validate that a validation message with text ‘ This field is required’ appears
+    under the date of birth box
+     */
+
+    @Test
+
+}
